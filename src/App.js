4f6c458762b6f02631cde7600a0c289742e6e99e
Elaine <elainefelipemoreira@gmail.com>
2022-05-28 19:33:45 -0300

    feat: 🎸 add config initial at project

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <img src="" className="App-logo" alt="em Manutenção" />
+        <p> 
+          Pagina <code>será uma</code> loja de vendas de landind pages.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/ElaineMoreira/sales_elaine"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Em construção
         </a>
       </header>
     </div>
