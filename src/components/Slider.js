2afd345065090c403850cebbcd20c6c30f54e57e
Elaine <elainefelipemoreira@gmail.com>
2022-05-28 21:30:44 -0300

    feat: 🎸 add images and card at Slider

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 
 function ImageSlider() {
+  let settings = {
+    dot:true,
+    Infinity:true,
+    speed: 500,
+    slideToShow:3,
+    slideToScroll:1,
+    cssEase:"linear"
+  }
   return(
-    <>
-    </>
+    <Slider {...settings}>
+      <div className='card-wrapper'>
+        <div className='card'>
+            <div className='card-image'>
+              <img src=""/>
+            </div>
+            <ul className='social-icons'>
+              <li><a href=''></a><i className='fa fa-instagram'></i></li>
+              <li><a href=''></a><i className='fa fa-github'></i></li>
+              <li><a href=''></a><i className='fa fa-linkedin'></i></li>
+            </ul>
+        </div>
+        
+      </div>
+      
+    </Slider>
   )
 }
 
