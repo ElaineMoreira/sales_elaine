a6b4058f59c398496585f8b332efe43050c7cb56
Elaine <elainefelipemoreira@gmail.com>
2022-05-29 11:05:09 -0300

    feat: 🎸 add style 1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import './App.css';
+import './css/App.css';
 import ImageSlider from './components/Slider';
 // https://www.npmjs.com/package/slick-carousel
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="" className="App-logo" alt="em Manutenção" />
-        <h1>Escolha sua Landind Page</h1>
-      </header>
+    <div className="Container mt-5 carousel">
+      <h1 className='slider-title'>Escolha sua Landind Page</h1>
       <ImageSlider />
     </div>
   );
--- src/components/Slider.js
+++ src/components/Slider.js
@@ -22,9 +22,9 @@ function ImageSlider() {
               <img src="https://github.com/ElaineMoreira/sales_elaine/blob/main/src/images/construcao.jpg?raw=true" alt='construcao'/>
             </div>
             <ul className='social-icons'>
-              {/* <li><a href='#'></a><i className='fa fa-instagram'></i></li>
+              <li><a href='#'></a><i className='fa fa-instagram'></i></li>
               <li><a href='#'></a><i className='fa fa-github'></i></li>
-              <li><a href='#'></a><i className='fa fa-linkedin'></i></li> */}
+              <li><a href='#'></a><i className='fa fa-linkedin'></i></li>
             </ul>
             <div className='details'>
               <h2><spam class='job-title'>Pages para Construção</spam></h2>
@@ -39,9 +39,9 @@ function ImageSlider() {
               <img src="https://github.com/ElaineMoreira/sales_elaine/blob/main/src/images/cosmeticos.jpg?raw=true" alt='cosmeticos'/>
             </div>
             <ul className='social-icons'>
-              {/* <li><a href='#'></a><i className='fa fa-instagram'></i></li>
+              <li><a href='#'></a><i className='fa fa-instagram'></i></li>
               <li><a href='#'></a><i className='fa fa-github'></i></li>
-              <li><a href='#'></a><i className='fa fa-linkedin'></i></li> */}
+              <li><a href='#'></a><i className='fa fa-linkedin'></i></li>
             </ul>
             <div className='details'>
               <h2><spam class='job-title'>Pages para Cosméticos</spam></h2>
@@ -56,9 +56,9 @@ function ImageSlider() {
               <img src="https://github.com/ElaineMoreira/sales_elaine/blob/main/src/images/hortifruti.jpg?raw=true" alt='hortifruti'/>
             </div>
             <ul className='social-icons'>
-              {/* <li><a href='#'></a><i className='fa fa-instagram'></i></li>
+              <li><a href='#'></a><i className='fa fa-instagram'></i></li>
               <li><a href='#'></a><i className='fa fa-github'></i></li>
-              <li><a href='#'></a><i className='fa fa-linkedin'></i></li> */}
+              <li><a href='#'></a><i className='fa fa-linkedin'></i></li>
             </ul>
             <div className='details'>
               <h2><spam class='job-title'>Pages para Hortifruti</spam></h2>
@@ -73,9 +73,9 @@ function ImageSlider() {
               <img src="https://github.com/ElaineMoreira/sales_elaine/blob/main/src/images/mercearia.jpg?raw=true" alt='hortifruti'/>
             </div>
             <ul className='social-icons'>
-              {/* <li><a href='#'></a><i className='fa fa-instagram'></i></li>
+              <li><a href='#'></a><i className='fa fa-instagram'></i></li>
               <li><a href='#'></a><i className='fa fa-github'></i></li>
-              <li><a href='#'></a><i className='fa fa-linkedin'></i></li> */}
+              <li><a href='#'></a><i className='fa fa-linkedin'></i></li>
             </ul>
             <div className='details'>
               <h2><spam class='job-title'>Pages para Mercearias</spam></h2>
@@ -90,9 +90,9 @@ function ImageSlider() {
               <img src="https://github.com/ElaineMoreira/sales_elaine/blob/main/src/images/roupas.jpg?raw=true" alt='roupas'/>
             </div>
             <ul className='social-icons'>
-              {/* <li><a href='#'></a><i className='fa fa-instagram'></i></li>
+              <li><a href='#'></a><i className='fa fa-instagram'></i></li>
               <li><a href='#'></a><i className='fa fa-github'></i></li>
-              <li><a href='#'></a><i className='fa fa-linkedin'></i></li> */}
+              <li><a href='#'></a><i className='fa fa-linkedin'></i></li>
             </ul>
             <div className='details'>
               <h2><spam class='job-title'>Pages para Roupas</spam></h2>
@@ -107,9 +107,9 @@ function ImageSlider() {
               <img src="https://github.com/ElaineMoreira/sales_elaine/blob/main/src/images/sapatos.jpg?raw=true" alt='sapatos'/>
             </div>
             <ul className='social-icons'>
-              {/* <li><a href='#'></a><i className='fa fa-instagram'></i></li>
+              <li><a href='#'></a><i className='fa fa-instagram'></i></li>
               <li><a href='#'></a><i className='fa fa-github'></i></li>
-              <li><a href='#'></a><i className='fa fa-linkedin'></i></li> */}
+              <li><a href='#'></a><i className='fa fa-linkedin'></i></li>
             </ul>
             <div className='details'>
               <h2><spam class='job-title'>Pages para Sapatos</spam></h2>
@@ -124,9 +124,9 @@ function ImageSlider() {
               <img src="https://github.com/ElaineMoreira/sales_elaine/blob/main/src/images/livros.jpg?raw=true" alt='livros'/>
             </div>
             <ul className='social-icons'>
-              {/* <li><a href='#'></a><i className='fa fa-instagram'></i></li>
+              <li><a href='#'></a><i className='fa fa-instagram'></i></li>
               <li><a href='#'></a><i className='fa fa-github'></i></li>
-              <li><a href='#'></a><i className='fa fa-linkedin'></i></li> */}
+              <li><a href='#'></a><i className='fa fa-linkedin'></i></li>
             </ul>
             <div className='details'>
               <h2><spam class='job-title'>Pages para Livros</spam></h2>
@@ -141,9 +141,9 @@ function ImageSlider() {
               <img src="https://github.com/ElaineMoreira/sales_elaine/blob/main/src/images/sapatos.jpg?raw=true" alt='sapatos'/>
             </div>
             <ul className='social-icons'>
-              {/* <li><a href='#'></a><i className='fa fa-instagram'></i></li>
+              <li><a href='#'></a><i className='fa fa-instagram'></i></li>
               <li><a href='#'></a><i className='fa fa-github'></i></li>
-              <li><a href='#'></a><i className='fa fa-linkedin'></i></li> */}
+              <li><a href='#'></a><i className='fa fa-linkedin'></i></li>
             </ul>
             <div className='details'>
               <h2><spam class='job-title'>Pages para Sapatos</spam></h2>
@@ -158,9 +158,9 @@ function ImageSlider() {
               <img src="https://github.com/ElaineMoreira/sales_elaine/blob/main/src/images/manicure.jpg?raw=true" alt='livros'/>
             </div>
             <ul className='social-icons'>
-              {/* <li><a href='#'></a><i className='fa fa-instagram'></i></li>
+              <li><a href='#'></a><i className='fa fa-instagram'></i></li>
               <li><a href='#'></a><i className='fa fa-github'></i></li>
-              <li><a href='#'></a><i className='fa fa-linkedin'></i></li> */}
+              <li><a href='#'></a><i className='fa fa-linkedin'></i></li>
             </ul>
             <div className='details'>
               <h2><spam class='job-title'>Pages para Manicure</spam></h2>
