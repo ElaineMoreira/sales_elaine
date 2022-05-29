68ee335778f3bde95afd8897d17b1c2e67debaad
Elaine <elainefelipemoreira@gmail.com>
2022-05-29 09:12:02 -0300

    feat: 🎸 add database and install uuid

---
+++ src/database/data.js
@@ -0,0 +1,12 @@
+import { v4 as uuidv4 } from 'uuid';
+
+export const projects = [
+  {
+    id: uuidv4(),
+    github: "https://github.com/ElaineMoreira"
+  },
+  {
+    id: uuidv4(),
+    linkedin: "https://www.linkedin.com/in/elaine-moreira/"   
+  },
+]
