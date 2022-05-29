3f23b0eda9264a6bfb6ca0d93c8fc51ec50ac087
Elaine <elainefelipemoreira@gmail.com>
2022-05-29 09:31:54 -0300

    feat: 🎸 add more photos and add dataImages

--- src/database/dataIcons.js
+++ src/database/dataIcons.js
@@ -3,10 +3,10 @@ import { v4 as uuidv4 } from 'uuid';
 export const projects = [
   {
     id: uuidv4(),
-    github: "https://github.com/ElaineMoreira"
+    url: "https://github.com/ElaineMoreira"
   },
   {
     id: uuidv4(),
-    linkedin: "https://www.linkedin.com/in/elaine-moreira/"   
+    url: "https://www.linkedin.com/in/elaine-moreira/"   
   },
 ]
---
+++ src/database/dataImages.js
@@ -0,0 +1,29 @@
+import { v4 as uuidv4 } from 'uuid';
+
+export const images = [
+  {
+    id: uuidv4(),
+    url: "https://github.com/ElaineMoreira/sales_elaine/blob/main/src/images/construcao.jpg?raw=true"
+  },
+  {
+    id: uuidv4(),
+    url: "https://github.com/ElaineMoreira/sales_elaine/blob/main/src/images/cosmeticos.jpg?raw=true"   
+  },
+  {
+    id: uuidv4(),
+    url: "https://github.com/ElaineMoreira/sales_elaine/blob/main/src/images/hortifruti.jpg?raw=true"   
+  },
+  {
+    id: uuidv4(),
+    url: "https://github.com/ElaineMoreira/sales_elaine/blob/main/src/images/mercearia.jpg?raw=true"   
+  },
+  {
+    id: uuidv4(),
+    url: "https://github.com/ElaineMoreira/sales_elaine/blob/main/src/images/roupas.jpg?raw=true"   
+  },
+  {
+    id: uuidv4(),
+    url: "https://github.com/ElaineMoreira/sales_elaine/blob/main/src/images/sapatos.jpg?raw=true"   
+  },
+]
+
